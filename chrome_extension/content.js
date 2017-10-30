b593599a6c4a9ce519de31a61e987d77196b5dd8
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-10-29 22:01:05 -0400

    Get title of the video
    Added a function to get the title of the video.

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,12 +1,20 @@
 // run the loop every 10 seconds
+getTitle();
 var loopTime = 10000;
 setInterval(timeStamp,loopTime);
+
+function getTitle() {
+    // get the title of the video being played
+    var videoTitle = document.title;
+    console.log(videoTitle);
+}
 
 // function to display the current time stamp on the console
 function timeStamp() {
     video = document.getElementsByClassName('video-stream')[0];
     // currentTime gives the current time stamp of the video
     console.log(video.currentTime);
+
     // call the function on "pause" event
     video.onpause = function() {
         console.log(video.currentTime);

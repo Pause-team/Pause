726b9d5def9f68af1705eb35e2f9a1cd5c3129ad
Anish <nkanish2002@gmail.com>
2017-11-10 17:02:32 -0500

    Adding documentation to validator.py

--- website/API/validators.py
+++ website/API/validators.py
@@ -1,3 +1,9 @@
+"""
+    Validator
+    ~~~~~~~~~
+
+    Regex for validation of various fields in the models.py.
+"""
 import re
 
 EMAIL_REGEX = re.compile('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$')

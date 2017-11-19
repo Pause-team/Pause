9f63b720742f171f01d9d11aeb471d24ee99d349
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-11-19 16:33:59 -0500

    Ajax call
    Added ajax call to send all the data to the php script.

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -22,21 +22,42 @@ function timeStamp() {
         console.log('Current Time Stamp: ' + video.currentTime);
 
         // setup variables to send to the php function
-        // the url of the video being watched
-        video_url = window.location.href;
-        // current placeholder - will change after getting this info form the login page
-        user_id = 1;
-        $.ajax({
-            type: "POST",
-            url: "https://localhost/Pause/pause-team.github.io/database/insertData.php",
-            data: {
-                'user_id': user_id,
-                'url': video_url,
-                'currentTime': video.currentTime
-            },
-            success: function(data) {
-                console.log(data);
-            }
+        // access the localstorage to get the user id,
+        // pass all the variables to the ajax url
+        chrome.storage.local.get('user_id', function(result) {
+            var user_id = result.user_id;
+            alert(user_id);
+
+            // the url of the video being watched
+            video_url = window.location.href;
+            // current placeholder - will change after getting this info from the login page
+            $.ajax({
+                type: "POST",
+                url: "https://localhost/Pause/pause-team.github.io/database/insertData.php",
+                data: {
+                    'user_id': user_id,
+                    'url': video_url,
+                    'currentTime': video.currentTime
+                },
+                success: function(data) {
+                    console.log(data);
+                }
+            });
         });
     };
 }
+
+window.addEventListener("message", function(event) {
+    // We only accept messages from ourselves
+    if (event.source != window)
+        return;
+
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        user_id = event.data.text;
+        alert(user_id);
+        // save the user id in the local storage of the extension
+        chrome.storage.local.set({
+            "user_id": user_id
+        });
+    }
+});

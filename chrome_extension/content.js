7506b9add68f2e6da57afc1b281d5e1898249095
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-11-19 14:27:57 -0500

    Ajax call to send data
    Send the required data as AJAX to the php handler

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,7 +1,7 @@
 // run the loop every 10 seconds
 // window.onload="getTitle()";
 var loopTime = 10000;
-setInterval(timeStamp,loopTime);
+setInterval(timeStamp, loopTime);
 
 // function to display the current time stamp on the console
 function timeStamp() {
@@ -20,5 +20,23 @@ function timeStamp() {
     video.onpause = function() {
         var videoTitle = document.getElementsByClassName('video-title');
         console.log('Current Time Stamp: ' + video.currentTime);
+
+        // setup variables to send to the php function
+        // the url of the video being watched
+        video_url = window.location.href;
+        // current placeholder - will change after getting this info form the login page
+        user_id = 1;
+        $.ajax({
+            type: "POST",
+            url: "https://localhost/Pause/pause-team.github.io/database/insertData.php",
+            data: {
+                'user_id': user_id,
+                'url': video_url,
+                'currentTime': video.currentTime
+            },
+            success: function(data) {
+                console.log(data);
+            }
+        });
     };
 }

32039278fd844d0faf86b1a09e42a7c1e1deae40
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-12-02 13:18:43 -0500

    Update URL
    Changed from creating a new tab for the profile.php to opening in the same Tab.

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -32,7 +32,7 @@ chrome.runtime.onMessage.addListener(
         chrome.storage.local.get('user_id', function(result) {
             var user_id = result.user_id;
             var newURL = "http://localhost/Pause/pause-team.github.io/profile.php?username=" + JSON.stringify(JSON.parse(user_id).uid);
-            chrome.tabs.create({
+            chrome.tabs.update({
                 url: newURL
             });;
         });

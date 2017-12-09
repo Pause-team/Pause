d4fa27a7890c10097c8f4cee5b32725dd4485ea8
swarnimsinha <31718689+swarnimsinha@users.noreply.github.com>
2017-12-09 13:57:47 -0500

    Change UID to Email
    Plugin will save the email as the user ID rather than the randomly generated UID

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -31,7 +31,7 @@ function timeStamp() {
         // access the localstorage to get the user id,
         // pass all the variables to the ajax url
         chrome.storage.local.get('user_id', function(result) {
-            var user_id = '"'+JSON.parse(result.user_id).uid+'"';
+            var user_id = '"'+JSON.parse(result.user_id).email+'"';
             var userName = JSON.parse(result.user_id).displayName;
             var emailId = JSON.parse(result.user_id).email;
             alert(userName);
@@ -41,7 +41,7 @@ function timeStamp() {
             // current placeholder - will change after getting this info from the login page
             $.ajax({
                 type: "POST",
-                url: "https://localhost/Pause/pause-team.github.io/database/insertData.php",
+                url: "https://pause.trixster.xyz/database/insertData.php",
                 data: {
                     'user_id': user_id,
                     'user_name': userName,
